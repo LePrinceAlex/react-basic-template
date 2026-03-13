274d6fe82a085f788f1468f78eb4b6b2077a0faa
Emgraphy <emilie.lawniczak@gmail.com>
2026-03-13 15:29:56 +0100

    Fin Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,13 @@
+
+import { Menu } from "lucide-react";
+import { X } from "lucide-react";
+import { useState } from "react";
+
+
 export default function Navbar() {
+    
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
     return (
         <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-rose-300/20 backdrop-blur-sm">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -14,8 +23,40 @@ export default function Navbar() {
                     </div>
 
                     {/* Nav Links */}
+                    <div className="hidden md:flex items-center space-x-6 lg:space-x-8">
+                        <a href="#features" className="text-pink-900 hover:text-pink-700 text-sm lg:text-base">Fonctionnalités</a>
+                        <a href="#prix" className="text-pink-900 hover:text-pink-700 text-sm lg:text-base">Prix</a>
+                        <a href="#temoignages" className="text-pink-900 hover:text-pink-700 text-sm lg:text-base">Témoignages</a>
+                    </div>
+
+                    <button className="md:hidden items-center px-2 text-pink-900 hover:text-pink-700" onClick={() => setMobileMenuOpen((prev) => !prev)}>
+                        {mobileMenuOpen ?
+                        <X className="w-5 h-5 sm:w-6 sm:h-6"/>
+                        :
+                        <Menu className="w-5 h-5 sm:w-6 sm:h-6"/>
+                        }
+                    </button>
                 </div>
             </div>
+
+            {mobileMenuOpen && 
+                <div className="md:hidden flex bg-rose-300/10 backdrop-blur-lg border-t border-rose-100 animate-in slide-in-from-top duration-300">
+                    <div className="px-4 py-4 sm:py-6 space-y-3 sm:space-y-4">
+                        <a 
+                            href="#features" 
+                            className="block text-pink-900 hover:text-pink-700 text-sm lg:text-base"
+                            onClick={() => setMobileMenuOpen(false)}>Fonctionnalités</a>
+                        <a 
+                            href="#prix" 
+                            className="block text-pink-900 hover:text-pink-700 text-sm lg:text-base"
+                            onClick={() => setMobileMenuOpen(false)}>Prix</a>
+                        <a 
+                            href="#temoignages" 
+                            className="block text-pink-900 hover:text-pink-700 text-sm lg:text-base"
+                            onClick={() => setMobileMenuOpen(false)}>Témoignages</a>
+                    </div>
+                </div>
+            }
         </nav>
     )
 }
